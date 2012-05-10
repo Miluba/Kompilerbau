7c4aa1adb99714b6992d1a0cd06edd175b1b4354
Johannes Merz <johannes-merz-2006@web.de>
2012-05-10 22:28:46 +0200

    Parser with arithmetic

--- workspace_comp/CompUeb1/src/classes/Parser.java
+++ workspace_comp/CompUeb1/src/classes/Parser.java
@@ -10,6 +10,8 @@ public class Parser {
 
 	private static Scanner scanner = null;
 	private static ScanOutput out = null;
+	
+
 
 	public static void main(String[] args) {
 		Parser parser = new Parser();
@@ -17,15 +19,15 @@ public class Parser {
 	}
 	
 	public void erweiterter_ausdruck(){
-		
+		double result;
 		scanner = new ScannerImpl();
 		out = scanner.scan();
 		
 		try {
-			this.ausdruck();
+				result = ausdruck();
 
 			if (out.getToken() == Scanner.ENDE) {
-				System.out.println("Eingabe syntaktisch richtig");
+				System.out.println("Eingabe syntaktisch richtig ::: Ergebnis: "+result);
 			} else {
 
 				throw new TerminationException("An Stelle " + scanner.getIndex()
@@ -36,74 +38,104 @@ public class Parser {
 			e.printStackTrace();
 		} catch (FactorException e) {
 			e.printStackTrace();
-
 		} catch (TerminationException e) {
 			e.printStackTrace();
 		}
 		
 	}
 
-	private void ausdruck() throws FactorException, CloseBracketException {
-
+	private double ausdruck() throws FactorException, CloseBracketException {
+		double sa2, sa3,sa4, ha4, ha6,ha7, ha8;
+		String ka1;
 		if (out.getToken() == Scanner.MINUS) {
+			
 			out = scanner.scan();
-			mal_durch_Term();
+			sa2 = mal_durch_Term();
+			ha4 = -sa2;
+			ha6=ha4;
 
-		} else
-			mal_durch_Term();
+		} else{
+			sa3 = mal_durch_Term();
+			ha6 = sa3;
+		}
 
 		while (out.getToken() == Scanner.PLUS
 				|| out.getToken() == Scanner.MINUS) {
+			
+			
+			ka1 = (out.getToken() == Scanner.PLUS) ? "+" : "-";
 			out = scanner.scan();
-			mal_durch_Term();
+			sa4 =mal_durch_Term();
+			ha7=sa4;
+			ha8 = (ka1.equals("+")) ? ha6+ha7 : ha6-ha7;
+			ha6=ha8;
 		}
-
+			return ha6;
 	}
 
-	private void mal_durch_Term() throws FactorException, CloseBracketException {
-		pot_Term();
+	private double mal_durch_Term() throws FactorException, CloseBracketException {
+		double sa3,sa4, ha6,ha7, ha8;
+		String ka1;
+		
+		sa3 = pot_Term();
+		ha6 = sa3;
 		while (out.getToken() == Scanner.MAL
 				|| out.getToken() == Scanner.GETEILT) {
-			out = scanner.scan();
-			pot_Term();
-		}
+			
 
+			ka1 = (out.getToken() == Scanner.GETEILT) ? "/" : "*";
+			out = scanner.scan();
+			sa4=pot_Term();
+			ha7 = sa4;
+			ha8 = (ka1.equals("/")) ? ha6/ha7 : ha6*ha7;
+			ha6=ha8;
+		}
+		return ha6;
 	}
 
-	private void pot_Term() throws FactorException, CloseBracketException {
-		faktor();
+	private double pot_Term() throws FactorException, CloseBracketException {
+		double sa3, sa4, ha6, ha8;
+		sa3=faktor();
+		ha6=sa3;
 
+		
 		if (out.getToken() == Scanner.HOCH) {
 			out = scanner.scan();
-			pot_Term();
+			sa4=pot_Term();
+			
+			ha8=Math.pow(sa4, sa3);
+			ha6=ha8; 
 		}
-
+		return ha6;
 	}
 
-	private void faktor() throws FactorException, CloseBracketException {
-
+	private double faktor() throws FactorException, CloseBracketException {
+		double sa3, ha6;
+		
 		if (out.getToken() == Scanner.KLAMMER_AUF) {
 			out = scanner.scan();
-			ausdruck();
+			sa3=ausdruck();
+			ha6=sa3;
 			if (out.getToken() == Scanner.KLAMMER_ZU) {
 				out = scanner.scan();
 			} else {
 
 				throw new CloseBracketException("An Stelle "
 						+ scanner.getIndex() + " steht " + out.getLexem()
-						+ " es wurde aber eine Klammer erwartet");
-
+						+ " es wurde aber eine schlie§ende Klammer erwartet");
 			}
 
 		} else if (out.getToken() == Scanner.ZAHL) {
+			ha6=Double.parseDouble(out.getLexem());
 			out = scanner.scan();
 		} else {
 
 			throw new FactorException("An Stelle " + scanner.getIndex()
 					+ " steht " + out.getLexem()
-					+ " es wurde aber eine Zahl erwartet");
+					+ " es wurde aber eine Zahl oder Klammer erwartet");
 
 		}
+		return ha6;
 	}
 
 }
